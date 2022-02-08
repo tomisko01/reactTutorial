1d269dcdf842232943b56137389d24da75e773fb
tomisko <toom134@gmail.com>
2022-02-08 20:18:23 +0100

    refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
-import EventComponent from "./events/EventComponent";
+import UserSearch from "./refs/UserSearch";
 
 const App = () => {
     return <div>
-        <EventComponent/>
+        <UserSearch/>
     </div>
 }
 
